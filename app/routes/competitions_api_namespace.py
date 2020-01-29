ef8e31e1d31351ebadc746afe9ed5c5097f6f52f
tlips <thomas.lips@ugent.be>
2020-01-29 16:49:16 +0100

    completed competitions namespace TODO: authorization

--- app/routes/competitions_api_namespace.py
+++ app/routes/competitions_api_namespace.py
@@ -1,4 +1,4 @@
-from flask_restplus import Namespace, Resource, fields
+from flask_restplus import Namespace, Resource, fields, abort
 from app.models.competition import Competition
 from app import db
 
@@ -10,11 +10,15 @@ competition_model = api.model('Competition', {
     'name': fields.String(required=True, description = "verbose identifier")
 })
 
-# request parser
+# post request parser
 
-competition_parser = api.parser()
-competition_parser.add_argument('name', type=str,required=True,help='name of the new competition')
+competition_post_parser = api.parser()
+competition_post_parser.add_argument('name', type=str, required=True, help='name of the new competition')
 
+# post get search param parser
+
+competition_get_parser = api.parser()
+competition_get_parser.add_argument('name', type=str, required=False, help='optional: name of the  competition ')
 @api.route('/')
 class CompetitionsList(Resource):
     @api.doc('List competitions')
@@ -24,9 +28,15 @@ class CompetitionsList(Resource):
         print(len(list))
         return list
 
-    @api.expect(competition_parser)
+    @api.expect(competition_post_parser)
     def post(self):
-        args = competition_parser.parse_args()
+        args = competition_post_parser.parse_args()
+        if Competition.query.filter_by(name=args.get('name')).scalar() is not None:
+            abort(422,"name already exists")
+
         c = Competition(name=args.get('name'))
         db.session.add(c)
         db.session.commit()
+        # create response
+        result = api.marshal(c,competition_model)
+        return result, 200
