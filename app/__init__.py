43a23ea309b461e99d49eca4bd2b5799233afc55
tlips <Thomas.Lips@UGent.be>
2020-03-31 23:29:13 +0200

    new environment strategy, as flask_environment is no longer maintained and contains bugs

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_environments import Environments
+import dotenv
 import os
 # globally accessible
 db = SQLAlchemy()
 
 
 def create_app():  # FACTORY!
+    #dotenv.load_dotenv()
+    #print(os.environ.get('DEBUG') == 'True')
     app = Flask(__name__)
 
     # dynamic configuration based on the FLASK_ENV system env variable
-    env = Environments(app)
-    env.from_object('config') # replaces the traditional flask config.from_object() and dynamically selects a subclass
-    print( f"env = {os.environ.get('FLASK_ENV')}")
+
+    # this function would load the env file in production as well,
+    # when running flask run, it is done automatically
+    # we don't need it as this is handled by docker compose
+    app.config.from_object('config.Config')
+
 
     # initialize plugins
     db.init_app(app)
--- config.py
+++ config.py
@@ -1,17 +1,13 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config(object):
-    DEBUG = True
+    '''
+    values should be specified in a .env file
+    '''
+    DEBUG = os.environ.get('DEBUG') == 'TRUE' or os.environ.get('DEBUG') == 'True'
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or "sqlite:///" + os.path.join(basedir,'app.db')
 
-
-class Development(Config):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "sqlite:///" + os.path.join(basedir,'app.db')
-
-
-class Production(Config):
-    DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'mlkqjsd5mfoiqjsdmlfkè4jqsdaqzàemlkifr)1nlodfmouut'
